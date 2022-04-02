99343267b996fa2b98c8e68c1108bd90e33ee45f
richharv557 <richharv557@gmail.com>
2022-04-01 21:14:25 -0500

    Added grid clear function and popup

--- javastuff.js
+++ javastuff.js
@@ -1,8 +1,10 @@
 const gridContainer = document.querySelector('#grid-container')
 
+createGrid(16);
+
+
 function createGrid(size) {
 // ID the container, create a column, append 16 divs, append to container
-
     for (let i = 0; i < size; i++){
         const gridColumn = document.createElement('div');
         gridColumn.classList.add('column');
@@ -12,8 +14,17 @@ function createGrid(size) {
             gridColumn.appendChild(gridItem);
         } gridContainer.appendChild(gridColumn);
     }
-
 }
 
+function clearGrid(parent) {
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
 
-
+function userPopUp() {
+    let input = prompt("Enter # of squares you want for the x & y axis. Limit 100.");
+        while (input == null || input < 0 || input > 100 || isNaN(input)) {
+            input = prompt("Invalid. Enter # of squares you want for the x & y axis. Limit 100.");
+        } return input;
+}
