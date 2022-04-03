b7e567e54bc9dde9e441d4fac34a2b6d1255afd6
richharv557 <richharv557@gmail.com>
2022-04-03 01:08:52 -0500

    Cleaned up readability and turned button listener into function.

--- javastuff.js
+++ javastuff.js
@@ -1,42 +1,49 @@
 //delaring this as a global since it never changes
 const gridContainer = document.querySelector('#grid-container')
-const button = document.querySelector('#btn')
 
 // initialize page per prompt
 createGrid(16);
+addButtonListener();
 
-button.addEventListener('click', () => {
+// Made this into a function to auto run on page startup to structure code more clearly.
+function addButtonListener() {
+  const button = document.querySelector('#btn')
+  button.addEventListener('click', () => {
     clearGrid(gridContainer);
     createGrid(userPopUp());
-})
+  });
+}
 
 // ID the container, create a column, append 16 divs with class .grid and add event listener as created, append to container
 function createGrid(size) {
-    for (let i = 0; i < size; i++){
-        const gridColumn = document.createElement('div');
-        gridColumn.classList.add('column');
-        for (let i = 0; i < size; i++) {
-            const gridItem = document.createElement('div');
-            gridItem.classList.add('grid');
-            gridItem.addEventListener('mouseover', () => {
-                gridItem.style.background = `black`;
-            })
-            gridColumn.appendChild(gridItem);
-        } gridContainer.appendChild(gridColumn);
-    }
+  for (let i = 0; i < size; i++){
+  const gridColumn = document.createElement('div');
+  gridColumn.classList.add('column');
+
+    for (let i = 0; i < size; i++) {
+      const gridItem = document.createElement('div');
+      gridItem.classList.add('grid');
+      gridItem.addEventListener('mouseover', () => {
+        gridItem.style.background = `black`;
+      });
+      gridColumn.appendChild(gridItem);
+    } 
+        
+    gridContainer.appendChild(gridColumn);
+  }
 }
 
 // clear grid function to remove all children from #grid-container
 function clearGrid(parent) {
-    while (parent.firstChild){
-        parent.removeChild(parent.firstChild);
+  while (parent.firstChild){
+    parent.removeChild(parent.firstChild);
     }
 }
 
 // validated popup that only accepts 0 - 100 as inputs
 function userPopUp() {
-    let input = prompt("Enter # of squares you want for the x & y axis. Limit 100.");
-        while (input == null || input < 0 || input > 100 || isNaN(input)) {
-            input = prompt("Invalid. Enter # of squares you want for the x & y axis. Limit 100.");
-        } return input;
+  let input = prompt("Enter # of squares you want for the x & y axis. Limit 100.");
+    while (input == null || input < 0 || input > 100 || isNaN(input)) {
+      input = prompt("Invalid. Enter # of squares you want for the x & y axis. Limit 100.");
+    } return input;
 }
